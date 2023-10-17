7345534cd766b47e35646d080dd3b81188b02be4
Ardian Kovanxhi <akovanxhi@gmail.com>
2023-10-16 21:30:12 -0400

    Time: 72 ms (57.43%), Space: 51.6 MB (57.49%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,50 +3,18 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minCostInd = 0;
-    let maxCostInd = 0;
-    let tempMinInd
+    let l = 0;
+    let r = 1;
+    let max = 0;
 
-    for (let i = 0; i < prices.length; i++) {
-        const val = prices[i];
-        const minVal = prices[minCostInd];
-        const maxVal = prices[maxCostInd];
-
-        if (val - prices[tempMinInd] > maxVal - minVal) {
-            minCostInd = tempMinInd
-            maxCostInd = i
+    while (r < prices.length) {
+        const sum = prices[r] - prices[l];
+        if (sum > max) max = sum
+        else if (sum < 0) {
+            l = r
+            r++
         }
-
-        if (maxCostInd && val < minVal) {
-            if (tempMinInd && prices[tempMinInd] < val) {
-                continue
-            }
-            tempMinInd = i
-            continue
-        }
-
-        if (val < minVal) {
-            minCostInd = i;
-            if (!maxCostInd || maxCostInd < i) maxCostInd = i
-        }
-
-        if (i > minCostInd && val > maxVal) maxCostInd = i
-    };
-    return prices[maxCostInd] - prices[minCostInd]
+        else r++
+    }
+    return max
 };
-
-// var maxProfit = function(prices) {
-//     let maxProfit = 0;
-//     let minPrice = prices[0];
-//     for (let i = 1; i < prices.length; i++) {
-//         const currentPrice = prices[i];
-//         const currentProfit = currentPrice - minPrice;
-//         if (currentProfit > maxProfit) {
-//             maxProfit = currentProfit;
-//         }
-//         if (currentPrice < minPrice) {
-//             minPrice = currentPrice;
-//         }
-//     }
-//     return maxProfit;
-// };

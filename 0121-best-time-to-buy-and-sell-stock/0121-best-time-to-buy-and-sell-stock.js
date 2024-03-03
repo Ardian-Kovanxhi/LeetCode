4e9ad85bd07e486fa64ef15e24d18c21fc6f2000
Ardian Kovanxhi <akovanxhi@gmail.com>
2024-03-02 19:06:48 -0500

    Time: 75 ms (52.87%), Space: 58.6 MB (61.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,26 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    
+    let maxProfit = 0;
+    
     let l = 0;
     let r = 1;
-    let max = 0;
 
     while (r < prices.length) {
-        const sum = prices[r] - prices[l];
-        if (sum > max) max = sum
-        else if (sum < 0) {
-            l = r
-            r++
+        const left = prices[l];
+        const right = prices[r];
+
+        if (right < left) {
+            l = r;
+            r++;
+            continue;
         }
-        else r++
+
+        const curr = right - left;
+        maxProfit = Math.max(maxProfit, curr);
+        r++;
     }
-    return max
+
+    return maxProfit;
 };

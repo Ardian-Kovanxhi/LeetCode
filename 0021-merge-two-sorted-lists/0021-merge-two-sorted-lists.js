cf223494b9c0eea44d9f5291438f622462279287
Ardian Kovanxhi <akovanxhi@gmail.com>
2024-04-13 21:09:48 -0400

    Time: 62 ms (58.01%), Space: 51.7 MB (47.59%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -10,19 +10,46 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+//  Definition for singly-linked list.
+//   function ListNode(val, next) {
+//       this.val = (val===undefined ? 0 : val)
+//       this.next = (next===undefined ? null : next)
+//   }
 var mergeTwoLists = function(list1, list2) {
-    let sentinel = tail = new ListNode();
-    while (list1 && list2) {
-        if (list1.val <= list2.val) {
-            tail.next = list1;
+     var res = new ListNode(0, null);
+
+    // Get a reference to the head of the merged list
+    var head = res;
+
+    // Traverse both lists and compare values at each step
+    while(list1 != null && list2 != null) {
+        if(list1.val <= list2.val) {
+            head.next = list1;
             list1 = list1.next;
-        }
-        else {
-            tail.next = list2;
+        } else {
+            head.next = list2;
             list2 = list2.next;
-        };
-        tail = tail.next;
+        }
+
+        // Move the head pointer of the merged list
+        head = head.next;
     }
-    tail.next = list1 || list2
-    return sentinel.next
-}
+
+    // Append the remaining nodes from list1
+    while(list1 != null) {
+        head.next = list1;
+        list1 = list1.next;
+        head = head.next;
+    }
+
+    // Append the remaining nodes from list2
+    while(list2 != null) {
+        head.next = list2;
+        list2 = list2.next;
+        head = head.next;
+    }
+
+    // Return the head of the merged list
+    return res.next;
+    
+};

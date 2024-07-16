3f71d206721967bec47dbe31b85524dd2ca607b0
Ardian Kovanxhi <akovanxhi@gmail.com>
2024-07-16 15:11:49 -0400

    Time: 56 ms (76.94%), Space: 17.8 MB (25.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         logger = {}
-        for i in range(len(nums)):
-            num = nums[i]
-            rem = target - num
-            if rem in logger:
-                return [logger[rem], i]
-            logger[num] = i
+
+        for i, v in enumerate(nums):
+            diff = target - v
+            if diff in logger : return [i, logger[diff]]
+            logger[v] = i

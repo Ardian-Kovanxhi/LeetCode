7a941749b325bbc5465c496618cb26622d9f965d
Ardian Kovanxhi <akovanxhi@gmail.com>
2024-07-27 18:48:20 -0400

    Time: 112 ms (10.38%), Space: 24 MB (8.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashLogger = {}
+        ans = collections.defaultdict(list)
 
-        for str in strs:
-            hash = [0] * 26
+        for word in strs:
+            hashkey = [0] * 26
 
-            for s in str:
-                hash[ord(s) - 97] += 1
+            for char in word:
+                hashkey[ord(char) - ord("a")] += 1
             
-            temp = tuple(hash)
-
-            if temp in hashLogger:
-                hashLogger[temp].append(str)
-            else:
-                hashLogger[temp] = [str]
-        return hashLogger.values()
+            ans[tuple(hashkey)].append(word)
+        print(ans)
+        
+        return ans.values()

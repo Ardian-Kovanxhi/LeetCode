76d0db3ab5ae295776350b1ab7358159a830cae9
Ardian Kovanxhi <akovanxhi@gmail.com>
2024-07-31 15:27:13 -0400

    Time: 186 ms (10.21%), Space: 18.1 MB (15.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         logger = {}
-        half = len(nums) // 2
 
         for num in nums:
             logger[num] = logger.get(num, 0) + 1
-            if logger[num] > half: return num
+
+            if logger[num] == math.ceil(len(nums) / 2): return num
